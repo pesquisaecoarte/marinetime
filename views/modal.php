a298edb1d864a0fa399b9534fb727f00ab4d0353
Ingrid Rodrigues <ingridcruz@infojr.com.br>
2017-10-09 12:06:23 -0300

    Alterações nas baes

---
+++ views/modal.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="../vendors/custombox-master/custombox.min.css" rel="stylesheet">
+	<script src="../vendors/custombox-master/custombox.min.js"></script>
+	<script src="../vendors/custombox-master/custombox.legacy.min.js"></script>
+
+
+</head>
+<body>
+<div id="demo-modal" class="demo-modal">
+  <a href="">
+  	
+  </a>
+  varias coisas aqui
+</div>
+</body>
+</html>
+
+<script type="text/javascript">
+var modal = new Custombox.modal({
+  content: {
+    effect: 'fadein',
+    target: '#modal'
+  }
+});
+
+// Open
+modal.open();
+</script>
